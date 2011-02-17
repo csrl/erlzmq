0b659dc15593b9b227adcd03dc49d7d3863f025f
Yurii Rashkovskii <yrashk@gmail.com>
2011-02-17 02:04:01 -0800

    Added initial version of performance tests

---
+++ perf/remote_thr.erl
@@ -0,0 +1,13 @@
+#! /usr/bin/env escript
+%%! -smp enable -pa ebin
+
+main([ConnectTo,MessageSizeStr,MessageCountStr]) ->
+    {MessageSize, _} = string:to_integer(MessageSizeStr),
+    {MessageCount, _} = string:to_integer(MessageCountStr),
+    {ok, Context} = zmq:init(1),
+    {ok, Socket} = zmq:socket(Context,pub),
+    zmq:connect(Socket, ConnectTo),
+    Msg = list_to_binary(lists:duplicate(MessageSize, 0)),
+    [ zmq:send(Socket, Msg) || _I <- lists:seq(1, MessageCount) ],
+    zmq:term(Context).
+    
